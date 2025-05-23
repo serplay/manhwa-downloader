566d864823a308d046bbbe27e863451bd2db21f3
Infinity <64598947+serplay@users.noreply.github.com>
2025-05-23 19:02:49 +0200

    added availableLanguages and cooking with pulling chapters

--- server/scraper.py
+++ server/scraper.py
@@ -20,15 +20,17 @@ def search(title, source):
                 comics = {}
                 for num,com in enumerate(data):
                     title = com["attributes"]["title"]
-                    id = com["id"]
+                    com_id = com["id"]
                     rel = com["relationships"]
+                    trans = com["attributes"]["availableTranslatedLanguages"]
                     for i in rel:
                         if i["type"] == "cover_art":
-                            cover_art = f'https://uploads.mangadex.org/covers/{id}/{i["attributes"]["fileName"]}.256.jpg'
+                            cover_art = f'https://uploads.mangadex.org/covers/{com_id}/{i["attributes"]["fileName"]}.256.jpg'
                             break
-                    comics[num] = {"id":com["id"], 
-                                   "title":com["attributes"]["title"], 
+                    comics[num] = {"id":com_id, 
+                                   "title":title, 
                                    "cover_art":cover_art, 
+                                   "availableLanguages":trans,
                                    }
                 return comics
         case 1: #Manghuas
@@ -45,12 +47,32 @@ def search(title, source):
             return
     return
 
-def get_chapters(title: str):
-    # Example response
-    return [
-        {"chapter": "Chapter 1", "url": "https://.../ch1"},
-        {"chapter": "Chapter 2", "url": "https://.../ch2"},
-    ]
+def get_chapters(id: str, source: int):
+    try:
+        source = int(source)
+    except ValueError:
+        return {"message": "Invalid source"}
+    match source:
+        case 0: #MangaDex
+            base_url = "https://api.mangadex.org"
+            r = req.get(f"{base_url}/manga/{id}/feed",
+                        params={
+                               }
+                        )
+            return r.content
+        case 1: #Manghuas
+            return
+        case 2: #Yakshascans
+            return
+        case 3: #Asurascan
+            return
+        case 4: #Kunmanga
+            return
+        case 5: #Toonily
+            return
+        case 6: #Toongod
+            return
+    return
 
 def search_chapters(chapters, keyword):
     return [
