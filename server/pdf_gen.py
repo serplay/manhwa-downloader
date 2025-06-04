2bfb6f8ecf36089b512edadc2961c6945571fc76
serplay <kuba20160101@gmail.com>
2025-06-04 13:33:37 +0200

    Add scraping functionality for Manhuaus chapters
    - Implemented image extraction from Manhuaus using BeautifulSoup.
    - Added handling for WEBP image conversion to JPEG format.
    - Enhanced chapter image storage and cleanup processes.
    - Zip Archaive is broken for the moment

--- server/pdf_gen.py
+++ server/pdf_gen.py
@@ -1,9 +1,12 @@
 import img2pdf
 import requests as req
+from bs4 import BeautifulSoup as bs
+import re
 import os
 import shutil
 import uuid
 from zipfile import ZipFile
+from PIL import Image
 
 
 def get_chapter_images(ids, source):
@@ -36,7 +39,19 @@ def get_chapter_images(ids, source):
                     chapters_zip.write(pdf, os.path.basename(pdf))
                     os.remove(pdf)
             return zip_path
-        case 1:  # Manghuas
+        case 1:  # Manhuaus
+            base_url = "https://manhuaus.com/manga/"
+            path = uuid.uuid4().hex
+            pdfs = []
+            os.makedirs(f"{path}", exist_ok=True)
+            for chap_id in ids:
+                chap_id, chap_num = chap_id.split("_")
+                r = req.get(f"{base_url}{chap_id}/")
+                soup = bs(r.text, "html.parser")
+                images = soup.find("div", {"class": "read-container"}).find_all("img")
+                image_links = [re.sub(r'[\t\r\n]',"",img["data-src"]) for img in images]
+                pdfs.append(gen_pdf(image_links, chap_num, path))
+                shutil.rmtree(f"{path}/{chap_num}")
             return
         case 2:  # Yakshascans
             return
@@ -54,13 +69,21 @@ def get_chapter_images(ids, source):
 def gen_pdf(images, chap_num, path):
     image_paths = []
     ch_path = f"{path}/{chap_num}"
+    os.makedirs(ch_path, exist_ok=True)
     for i, img_url in enumerate(images):
         extension = img_url.split(".")[-1]
         img_data = req.get(img_url).content
         img_path = f"{ch_path}/{i}.{extension}"
         with open(img_path, "wb") as img_file:
             img_file.write(img_data)
-        image_paths.append(img_path)
+        if extension.lower() == "webp":
+            # Convert WEBP to JPEG
+            im = Image.open(img_path).convert("RGB")
+            im.save(f'{img_path}.jpg', "JPEG") # Save as JPEG
+            os.remove(img_path)  # Remove the original image file
+            image_paths.append(f'{img_path}.jpg')
+        else:
+            image_paths.append(img_path)
     title = f"Chapter_{chap_num}"
     pdf_path = f"{path}/{title}.pdf"
     with open(pdf_path, "wb") as f:
