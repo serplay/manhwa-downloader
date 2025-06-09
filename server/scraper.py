96dfe5403ce2d2f022034f6d0687a4db3def3cbf
serplay <kuba20160101@gmail.com>
2025-06-09 22:08:46 +0200

    test if httpx will fight cloudflare

--- server/scraper.py
+++ server/scraper.py
@@ -5,8 +5,44 @@ from fastapi import FastAPI, Response
 from fastapi.responses import StreamingResponse
 import io
 import os
+import httpx
+import ssl
 
 ROOT_URL = os.environ["ROOT_URL"]
+
+ssl_ctx = ssl.SSLContext(protocol=ssl.PROTOCOL_TLSv1_2)
+ssl_ctx.set_alpn_protocols(["h2", "http/1.1"])
+ssl_ctx.set_ecdh_curve("prime256v1")
+ssl_ctx.set_ciphers(
+    "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:"
+    "TLS_AES_128_GCM_SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:"
+    "ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:"
+    "ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:"
+    "DHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:"
+    "ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES128-GCM-SHA256:"
+    "ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:"
+    "DHE-RSA-AES256-SHA256:ECDHE-ECDSA-AES128-SHA256:"
+    "ECDHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA256:"
+    "ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:"
+    "DHE-RSA-AES256-SHA:ECDHE-ECDSA-AES128-SHA:"
+    "ECDHE-RSA-AES128-SHA:DHE-RSA-AES128-SHA:"
+    "RSA-PSK-AES256-GCM-SHA384:DHE-PSK-AES256-GCM-SHA384:"
+    "RSA-PSK-CHACHA20-POLY1305:DHE-PSK-CHACHA20-POLY1305:"
+    "ECDHE-PSK-CHACHA20-POLY1305:AES256-GCM-SHA384:"
+    "PSK-AES256-GCM-SHA384:PSK-CHACHA20-POLY1305:"
+    "RSA-PSK-AES128-GCM-SHA256:DHE-PSK-AES128-GCM-SHA256:"
+    "AES128-GCM-SHA256:PSK-AES128-GCM-SHA256:AES256-SHA256:"
+    "AES128-SHA256:ECDHE-PSK-AES256-CBC-SHA384:"
+    "ECDHE-PSK-AES256-CBC-SHA:SRP-RSA-AES-256-CBC-SHA:"
+    "SRP-AES-256-CBC-SHA:RSA-PSK-AES256-CBC-SHA384:"
+    "DHE-PSK-AES256-CBC-SHA384:RSA-PSK-AES256-CBC-SHA:"
+    "DHE-PSK-AES256-CBC-SHA:AES256-SHA:PSK-AES256-CBC-SHA384:"
+    "PSK-AES256-CBC-SHA:ECDHE-PSK-AES128-CBC-SHA256:ECDHE-PSK-AES128-CBC-SHA:"
+    "SRP-RSA-AES-128-CBC-SHA:SRP-AES-128-CBC-SHA:RSA-PSK-AES128-CBC-SHA256:"
+    "DHE-PSK-AES128-CBC-SHA256:RSA-PSK-AES128-CBC-SHA:"
+    "DHE-PSK-AES128-CBC-SHA:AES128-SHA:PSK-AES128-CBC-SHA256:PSK-AES128-CBC-SHA"
+)
+headers = {"User-agent": 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_5_1; like Mac OS X) AppleWebKit/534.11 (KHTML, like Gecko) Chrome/50.0.3433.281 Mobile Safari/603.0'}
 
 def proxy_image(url: str):
     try:
@@ -56,11 +92,13 @@ def search(title, source):
                 return comics
         case 1: #Manhuaus
             base_url = "https://manhuaus.com"
-            r = req.get(base_url,
-                        params={
-                            "s":title,
-                            "post_type":"wp-manga"
-                        })
+            #r = req.get(base_url,
+            #            params={
+            #                "s":title,
+            #                "post_type":"wp-manga"
+            #            })
+            client = httpx.Client(http2=True, verify=ssl_ctx)
+            r = client.get(f"{base_url}/?s={title}&post_type=wp-manga", headers=headers)
             soup = bs(r.text, "html.parser")
             if "Just a moment" in soup.text or "Please wait while we are checking your browser..." in soup.text:
                 return {"message": "Cloudflare challenge failed."}
