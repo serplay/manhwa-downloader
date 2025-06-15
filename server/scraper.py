c0a9d123308e41b7baf236cb6125a43928c711e4
serplay <kuba20160101@gmail.com>
2025-06-15 14:17:12 +0000

    Enhance MangaReader support in scraper.py
    - Updated error handling for data fetching from MangaReader. (fixed typo)
    - Implemented chapter data parsing for MangaReader, allowing retrieval of chapter information from the site.

--- server/scraper.py
+++ server/scraper.py
@@ -157,7 +157,7 @@ def search(title, source):
                             })
                 r.raise_for_status()
             except req.RequestException as e:
-                raise Exception(f"Failed to fetch data from Manhuaus: {e}")
+                raise Exception(f"Failed to fetch data from Mangareader: {e}")
 
             soup = bs(r.text, 'html.parser')
 
@@ -167,7 +167,7 @@ def search(title, source):
                 comics = {}
                 for num,com in enumerate(soup.find_all("div",{"class":"item item-spc"})):
                     languages = com.find("span",{"class":"tick tick-item tick-lang"}).contents[0].lower().split("/")
-                    com_id = 'https://mangareader.to' + com.find("a",{'class':"manga-poster"})['href']
+                    com_id = com.find("a",{'class':"manga-poster"})['href']
                     cover_art = com.a.img['src']
                     title = {'en': com.find("h3").a.contents[0]}
                     comics[num] = {"id": com_id,
@@ -313,7 +313,31 @@ def get_chapters(id: str, source: int):
             
             return data
         case 9:  # Mangareader
-            raise NotImplementedError("Pulling chapters from Mangareader is not implemented yet.")
+            base_url = f"https://mangareader.to"
+            try:
+                r = req.get(f'{base_url}{id}')
+                r.raise_for_status()
+            except req.RequestException as e:
+                raise Exception(f"Failed to fetch data from Mangareader: {e}")
+
+            soup = bs(r.text, 'html.parser')
+
+            try:
+                if "Just a moment" in soup.text or "Please wait while we are checking your browser..." in soup.text:
+                    raise Exception("Cloudflare challenge failed.")
+                
+                data = {"Vol 1":{ "volume": "Vol 1", "chapters": {}}}
+                for num, chap in enumerate(soup.find_all("li", {"class":"item reading-item chapter-item"})):
+                    data["Vol 1"]["chapters"][str(num)] = {
+                        "id": chap.a["href"].split("/")[-1],
+                        "chapter": chap["data-number"]
+                    }
+                    
+                return data
+                
+            except Exception as e:
+                raise Exception(f"Error parsing response from MangaReader: {e}")
+            
         case 10:  # Mangasee123
             raise NotImplementedError("Pulling chapters from Mangasee123 is not implemented yet.")
         case _:
