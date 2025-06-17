5ded9f69949e169410c8f99741b185cd9158bfe6
serplay <kuba20160101@gmail.com>
2025-06-17 03:01:57 +0000

    change of sources order

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -310,16 +310,16 @@ function App() {
             className="p-3 rounded-xl border border-gray-200 dark:border-[#2e2b40] bg-white dark:bg-[#1c1b29] text-gray-900 dark:text-[#f4f4ff] hover:outline-none focus:outline-none focus:border-pink-500 dark:focus:border-violet-500"
           >
             <option value="0">MangaDex</option>
+            <option value="9">Bato</option>
+            <option value="8">Mangapill</option>
+            <option value="7">Mangahere</option>
+            <option value="10">Weebcentral</option>
             <option value="1">Manhuaus</option>
             <option value="2">Yakshascans</option>
             <option value="3">Asurascan</option>
             <option value="4">Kunmanga</option>
             <option value="5">Toonily</option>
             <option value="6">Toongod</option>
-            <option value="7">Mangahere</option>
-            <option value="8">Mangapill</option>
-            <option value="9">Bato</option>
-            <option value="10">Weebcentral</option>
           </select>
           <button
             onClick={handleSearch}
