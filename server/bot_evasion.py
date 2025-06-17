a6daa9053828c8670bd105ebc8013b76a86c2cca
serplay <kuba20160101@gmail.com>
2025-06-17 02:13:43 +0000

    Added print statements to look for errors
    - whole backend on render halts on manhuaus XDDDD, I need to see where

--- server/bot_evasion.py
+++ server/bot_evasion.py
@@ -2,9 +2,13 @@ from seleniumbase import SB
 
 def get_with_captcha(url, elem, click=False):
     with SB(uc=True, xvfb=True) as sb:
+        print("bim")
         sb.uc_open_with_reconnect(url, 4)
+        print("bam")
         sb.uc_gui_click_captcha()
+        print("bum")
         if elem:
+            print("Waiting for element:", elem)
             try:
                 sb.wait_for_element(elem, timeout=10)
             except Exception as e:
