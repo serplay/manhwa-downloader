41a7a57e1f0aa59baaba02cc5ee8c9df39d2fb2b
serplay <kuba20160101@gmail.com>
2025-06-17 03:03:01 +0000

    Added new get_with_captcha_simple function in bot_evasion.py and updated scraper.py to use this function for fetching data from Manhuaus.

--- server/bot_evasion.py
+++ server/bot_evasion.py
@@ -17,3 +17,21 @@ def get_with_captcha(url, elem, click=False):
             sb.click('button:contains("Show All Chapters")')
         soup = sb.get_beautiful_soup()
     return soup
+
+def get_with_captcha_simple(url, elem, click=False):
+    with SB(uc=True, xvfb=True) as sb:
+        print("bim")
+        sb.uc_open(url)
+        print("bam")
+        sb.uc_gui_click_captcha()
+        print("bum")
+        if elem:
+            print("Waiting for element:", elem)
+            try:
+                sb.wait_for_element(elem, timeout=10)
+            except Exception as e:
+                raise Exception()
+        if click:
+            sb.click('button:contains("Show All Chapters")')
+        soup = sb.get_beautiful_soup()
+    return soup
