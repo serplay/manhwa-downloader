cde29b2c3f7a9d7fe68b933a672e642e0d9c655c
serplay <kuba20160101@gmail.com>
2025-06-25 23:18:19 +0000

    feat: fully implemented CBZ file format support
    - Added support for generating CBZ files using chapter images and after trial and error, it works ig.
    - Never used this format but hopefully for comic readers it will be blessing 🙏

--- server/Formats/cbz.py
+++ server/Formats/cbz.py
@@ -1,33 +1,72 @@
-import shutil
+from pathlib import Path
+from zipfile import ZipFile
 import os
+import shutil
+from math import ceil
+import re
+from cbz.comic import ComicInfo
+from cbz.constants import PageType, YesNo, Manga, AgeRating, Format
+from cbz.page import PageInfo
 
-def gen_cbz(images, chap_num, path, referer=None):
+def gen_cbz(path, update_progress=None, comic_title="Comic"):
     """
-    Downloads images and generates a CBZ for a manga chapter.
+    Generates a CBZ files for a manga based on the downloaded images.
 
     Args:
-        images (list): List of image URLs or tuples (url, referer) if referer is used.
-        chap_num (str): Chapter number, used for naming the output CBZ and directory.
-        path (str): Directory where the chapter folder and CBZ will be saved.
-        referer (str, optional): If provided, sets the HTTP Referer header for image requests.
+        path (str): Path to the target directory.
+        update_progress (Optional[Callable]): Callback function to update progress, if available.
 
     Returns:
         str: Path to the generated CBZ file.
-
-    Behavior:
-        - Downloads each image, handling referer headers if needed.
-        - Converts WEBP images to JPEG.
-        - Skips images smaller than 72x72 pixels.
-        - If an image fails to download, uses a placeholder 'corrupt.png'.
-        - Cleans up temporary images if an error occurs.
     """
-    
-    image_paths = []
-    ch_path = f"{path}/{chap_num}"
-    os.makedirs(ch_path, exist_ok=True)
-    
     try:
-        return
+        chapter_paths = [f.path for f in os.scandir(path) if f.is_dir()]
+        total_chapters = len(chapter_paths)
+
+        if update_progress:
+                update_progress(total_chapters, f"Creating CBZ...")
+
+        chapters = []
+        for i,chapter in enumerate(chapter_paths):
+            chap_num = os.path.basename(chapter)
+            if update_progress:
+                update_progress(total_chapters, f"Processing chapter {chap_num}...")
+            images = sorted([img.path for img in os.scandir(chapter) if img.is_file()],key=lambda p: int(re.search(r"(\d+)", os.path.basename(p)).group()))
+            pages = [
+                PageInfo.load(
+                    path=img_path,
+                    type=PageType.FRONT_COVER if i == 0 else PageType.BACK_COVER if i == len(images) - 1 else PageType.STORY,
+                )
+                for i, img_path in enumerate(images)
+            ]
+
+            comic = ComicInfo(
+                pages=pages,
+                title="Chapter " + chap_num,
+                serites=comic_title,
+                manga=Manga.NO,
+                number=i+1,
+                language_iso="en",
+                format=Format.WEB_COMIC,
+                black_and_white=YesNo.NO,
+                age_rating=AgeRating.UNKNOWN,
+            )
+
+            cbz_content = comic.pack()
+            cbz_path = Path(path) / f"Chapter {chap_num}.cbz"
+            chapters.append(cbz_path)
+            cbz_path.write_bytes(cbz_content)
+            shutil.rmtree(chapter,ignore_errors=True)
+
+
+        with ZipFile(f"{path}/Chapters.zip", 'w') as zipf:
+            for cbz in chapters:
+                zipf.write(cbz, os.path.basename(cbz))
+                os.remove(cbz)
     except Exception as e:
-        shutil.rmtree(ch_path, ignore_errors=True)
         raise e
+
+
+    return f"{path}/Chapters.zip"
+    
+    
